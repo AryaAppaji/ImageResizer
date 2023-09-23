d86b29439b3357b54bb8bab0dabc9659b672d423
AryaAppaji <maappaji111@gmail.com>
2023-09-23 21:50:29 +0530

    Chages in alignment, solved scrollbar problem, customized error messages.

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -8,9 +8,14 @@ class UploadController extends Controller
 {
     public function upload(Request $request){
         $request->validate([
-            "upload" => ["required","mimes:jpg,jpeg,png,avif"],
+            "upload" => ["required","mimes:jpg,jpeg,png,avif,gif,webp"],
             "width" => ["required"],
             "height" => ["required"],
+        ],
+        [
+            "upload.required" => "Please select a file.",
+            "width" => "Please enter a value.",
+            "height" => "Please enter a value.",
         ]);
         $filename = $request->file("upload")->getClientOriginalName();
         $request->file("upload")->storeAs("public",$filename);
